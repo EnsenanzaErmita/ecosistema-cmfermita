4b36737030368ede4616ef88274c765dd1233fcd
EnsenanzaErmita <manuel.gonzalezz@issste.gob.mx>
2026-06-29 08:29:54 -0600

    Add files via upload
    v19.0

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('--- ¡ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO CON NODEMAILER COMPILANDO! ---');
+console.log('--- ¡ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO CON NODEMAILER COMPILANDO 19! ---');
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
