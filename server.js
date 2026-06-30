a569c339369a4e0f8de32cba0ed3735252996b19
EnsenanzaErmita <manuel.gonzalezz@issste.gob.mx>
2026-06-30 09:34:02 -0600

    Add files via upload
    v37.8

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO 32.7.0');
+console.log('ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO 32.8.0');
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -432,7 +432,7 @@ app.put('/api/office-changes/:id', (req, res) => {
                             Con el objetivo de seguir mejorando la calidad en la atención de nuestra unidad médica, le solicitamos de la manera más atenta apoyarnos a responder una breve encuesta de Trato Digno.
                         </p>
                         <!-- BOTÓN BLINDADO CON ATRIBUTOS DE SEGURIDAD PARA ENLACES -->
-                        <a href="https://onrender.com" target="_blank" rel="noopener noreferrer" style="background-color: #611232; color: white; border: 1px solid #b38e5d; padding: 10px 20px; text-decoration: none; border-radius: 4px; font-weight: bold; font-size: 0.9em; display: inline-block; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
+                        <a href="https://encuestas-ermita-salud-mental.onrender.com/" target="_blank" rel="noopener noreferrer" style="background-color: #611232; color: white; border: 1px solid #b38e5d; padding: 10px 20px; text-decoration: none; border-radius: 4px; font-weight: bold; font-size: 0.9em; display: inline-block; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
                             Responder Encuesta de Trato Digno
                         </a>
                     </div>
