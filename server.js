6fa2ee40abc0713acd107f20b68a5b12faab5afd
EnsenanzaErmita <manuel.gonzalezz@issste.gob.mx>
2026-07-07 11:30:15 -0600

    Add files via upload
    V6.0

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO 5.0.0');
+console.log('ESTA ES LA VERSIÓN NUEVA DEL ARCHIVO 6.0.0');
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
